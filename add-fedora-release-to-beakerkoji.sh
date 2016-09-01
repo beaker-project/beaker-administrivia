75411295e99b527ca9d5c34b8485bb78a2355d1e
Dan Callaghan <dcallagh@redhat.com>
2016-09-01 15:47:08 +1000

    wrong inheritance on -redhat-testing tags

--- add-fedora-release-to-beakerkoji.sh
+++ add-fedora-release-to-beakerkoji.sh
@@ -21,7 +21,7 @@ fi
 koji -p beakerkoji add-tag beaker-client-fedora-$REL
 koji -p beakerkoji add-tag beaker-client-fedora-$REL-testing --parent beaker-client-fedora-$REL
 koji -p beakerkoji add-tag beaker-client-fedora-$REL-redhat --parent beaker-client-fedora-$REL
-koji -p beakerkoji add-tag beaker-client-fedora-$REL-redhat-testing --parent beaker-client-fedora-$REL-testing --parent beaker-client-fedora-$REL
+koji -p beakerkoji add-tag beaker-client-fedora-$REL-redhat-testing --parent beaker-client-fedora-$REL-testing --parent beaker-client-fedora-$REL-redhat
 koji -p beakerkoji add-tag beaker-client-fedora-$REL-build --parent beaker-client-fedora-$REL --arches=i686,x86_64
 koji -p beakerkoji add-target beaker-client-fedora-$REL-testing beaker-client-fedora-$REL-build beaker-client-fedora-$REL-testing
 koji -p beakerkoji add-target beaker-client-fedora-$REL-redhat-testing beaker-client-fedora-$REL-build beaker-client-fedora-$REL-redhat-testing
@@ -38,7 +38,7 @@ koji -p beakerkoji tag-pkg --nowait beaker-client-fedora-$REL-build rhpkg-simple
 koji -p beakerkoji add-tag beaker-harness-fedora-$REL
 koji -p beakerkoji add-tag beaker-harness-fedora-$REL-testing --parent beaker-harness-fedora-$REL
 koji -p beakerkoji add-tag beaker-harness-fedora-$REL-redhat --parent beaker-harness-fedora-$REL
-koji -p beakerkoji add-tag beaker-harness-fedora-$REL-redhat-testing --parent beaker-harness-fedora-$REL-testing --parent beaker-harness-fedora-$REL
+koji -p beakerkoji add-tag beaker-harness-fedora-$REL-redhat-testing --parent beaker-harness-fedora-$REL-testing --parent beaker-harness-fedora-$REL-redhat
 koji -p beakerkoji add-tag beaker-harness-fedora-$REL-build --parent beaker-harness-fedora-$REL --arches=i686,x86_64
 koji -p beakerkoji add-target beaker-harness-fedora-$REL beaker-harness-fedora-$REL-build beaker-harness-fedora-$REL
 koji -p beakerkoji add-target beaker-harness-fedora-$REL-testing beaker-harness-fedora-$REL-build beaker-harness-fedora-$REL-testing
